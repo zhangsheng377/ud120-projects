d2a36ea1b055b9619d568b7a7f170605c9c8a1bf
zhangsheng377 <435878393@qq.com>
2017-11-14 03:47:46 +0800

    适配了python3，urllib的下载部分做了进度显示

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -9,25 +9,25 @@
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
+
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
+
+clf = GaussianNB()
+clf.fit(features_train, labels_train)
+print(clf.score(features_test, labels_test))
 
 
 #########################################################
-
-
--- tools/startup.py
+++ tools/startup.py
@@ -1,47 +1,58 @@
 #!/usr/bin/python
 
-print
-print "checking for nltk"
+print()
+print("checking for nltk")
 try:
     import nltk
 except ImportError:
-    print "you should install nltk before continuing"
+    print("you should install nltk before continuing")
 
-print "checking for numpy"
+print("checking for numpy")
 try:
     import numpy
 except ImportError:
-    print "you should install numpy before continuing"
+    print("you should install numpy before continuing")
 
-print "checking for scipy"
+print("checking for scipy")
 try:
     import scipy
 except:
-    print "you should install scipy before continuing"
+    print("you should install scipy before continuing")
 
-print "checking for sklearn"
+print("checking for sklearn")
 try:
     import sklearn
 except:
-    print "you should install sklearn before continuing"
+    print("you should install sklearn before continuing")
 
-print
-print "downloading the Enron dataset (this may take a while)"
-print "to check on progress, you can cd up one level, then execute <ls -lthr>"
-print "Enron dataset should be last item on the list, along with its current size"
-print "download will complete at about 423 MB"
-import urllib
+print()
+print("downloading the Enron dataset (this may take a while)")
+print("to check on progress, you can cd up one level, then execute <ls -lthr>")
+print("Enron dataset should be last item on the list, along with its current size")
+print("download will complete at about 423 MB")
+import urllib.request
+
+
+def report_hook(blocks_read, block_size, total_size):
+    if not blocks_read:
+        print("Connection opened")
+    if total_size < 0:
+        print("Read %d blocks" % blocks_read)
+    else:
+        print("downloading: %d KB, totalsize: %d KB" % (blocks_read * block_size / 1024.0, total_size / 1024.0))
+
+
 url = "https://www.cs.cmu.edu/~./enron/enron_mail_20150507.tar.gz"
-urllib.urlretrieve(url, filename="../enron_mail_20150507.tar.gz") 
-print "download complete!"
+urllib.request.urlretrieve(url, filename="../enron_mail_20150507.tar.gz", reporthook=report_hook)
+print("download complete!")
 
-
-print
-print "unzipping Enron dataset (this may take a while)"
+print()
+print("unzipping Enron dataset (this may take a while)")
 import tarfile
 import os
+
 os.chdir("..")
 tfile = tarfile.open("enron_mail_20150507.tar.gz", "r:gz")
 tfile.extractall(".")
 
-print "you're ready to go!"
+print("you're ready to go!")
