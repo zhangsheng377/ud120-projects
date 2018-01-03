71818bc371ca2a7e2776ab546d392b798ab6df58
zhangsheng377 <435878393@qq.com>
2018-01-04 02:15:22 +0800

    完成课程9聚类

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -4,17 +4,13 @@
     Skeleton code for k-means clustering mini-project.
 """
 
-
-
-
 import pickle
 import numpy
 import matplotlib.pyplot as plt
 import sys
+
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
-
-
 
 
 def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature 1", f2_name="feature 2"):
@@ -24,7 +20,7 @@ def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature
     ### drawing more than five clusters
     colors = ["b", "c", "k", "m", "g"]
     for ii, pp in enumerate(pred):
-        plt.scatter(features[ii][0], features[ii][1], color = colors[pred[ii]])
+        plt.scatter(features[ii][0], features[ii][1], color=colors[pred[ii]])
 
     ### if you like, place red stars over points that are POIs (just for funsies)
     if mark_poi:
@@ -37,40 +33,59 @@ def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature
     plt.show()
 
 
-
 ### load in the dict of dicts containing all the data on each person in the dataset
-data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
 ### there's an outlier--remove it! 
 data_dict.pop("TOTAL", 0)
+# print(data_dict['GLISAN JR BEN F'].keys())
+min_eso = 9999999999999
+max_eso = -1
+min_sal = 9999999999999
+max_sal = -1
+for key in data_dict.keys():
+    if data_dict[key]['exercised_stock_options'] != 'NaN':
+        if data_dict[key]['exercised_stock_options'] > max_eso:
+            max_eso = data_dict[key]['exercised_stock_options']
+        if data_dict[key]['exercised_stock_options'] < min_eso:
+            min_eso = data_dict[key]['exercised_stock_options']
+    if data_dict[key]['salary'] != 'NaN':
+        if data_dict[key]['salary'] > max_sal:
+            max_sal = data_dict[key]['salary']
+        if data_dict[key]['salary'] < min_sal:
+            min_sal = data_dict[key]['salary']
+print('min of the exercised_stock_options is', min_eso)
+print('max of the exercised_stock_options is', max_eso)
+print('min of the salary is', min_sal)
+print('max of the salary is', max_sal)
 
-
-### the input features we want to use 
+### the input features we want to use
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
-poi  = "poi"
-features_list = [poi, feature_1, feature_2]
-data = featureFormat(data_dict, features_list )
-poi, finance_features = targetFeatureSplit( data )
-
+feature_3 = "total_payments"
+poi = "poi"
+features_list = [poi, feature_1, feature_2, feature_3]
+data = featureFormat(data_dict, features_list)
+poi, finance_features = targetFeatureSplit(data)
 
 ### in the "clustering with 3 features" part of the mini-project,
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2 in finance_features:
-    plt.scatter( f1, f2 )
-plt.show()
+'''for f1, f2 in finance_features:
+    plt.scatter(f1, f2)
+plt.show()'''
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
+from sklearn.cluster import KMeans
 
-
-
+kmeans = KMeans(n_clusters=2).fit(finance_features)
+pred = kmeans.labels_
 
 ### rename the "name" parameter when you change the number of features
 ### so that the figure gets saved to a different file
 try:
-    Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
+    Draw(pred, finance_features, poi, mark_poi=True, name="clusters_3feature.png", f1_name=feature_1, f2_name=feature_2)
 except NameError:
-    print "no predictions object named pred found, no clusters to plot"
+    print("no predictions object named pred found, no clusters to plot")
--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -10,7 +10,7 @@ from feature_format import featureFormat, targetFeatureSplit
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
 features = ["salary", "bonus"]
-data_dict.pop('TOTAL',0)
+data_dict.pop('TOTAL', 0)
 data = featureFormat(data_dict, features)
 
 ### your code below
@@ -20,11 +20,10 @@ for point in data:
     matplotlib.pyplot.scatter(salary, bonus)
 
 for key in data_dict.keys():
-    if data_dict[key]['bonus']!= 'NaN' and data_dict[key]['bonus']>90000000:
+    if data_dict[key]['bonus'] != 'NaN' and data_dict[key]['bonus'] > 90000000:
         print(key)
-    if data_dict[key]['bonus']!= 'NaN' and data_dict[key]['bonus']>5000000:
-        print(key,data_dict[key]['salary'])
-
+    if data_dict[key]['bonus'] != 'NaN' and data_dict[key]['bonus'] > 5000000:
+        print(key, data_dict[key]['salary'])
 
 matplotlib.pyplot.xlabel("salary")
 matplotlib.pyplot.ylabel("bonus")
